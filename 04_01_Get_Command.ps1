a2d8758c1762869aa2262cf4d99da31383c4d0d0
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-11-29 05:44:37 +0100

    Add files via upload

---
+++ 04_01_Get_Command.ps1
@@ -0,0 +1,26 @@
+#this is walk through demo file - not a script"
+Clear-Host
+#verbs
+Get-Verb
+Get-Command -verb Get
+Get-Command -verb Stop
+
+#nouns
+Get-Command -noun time*
+Get-Command -noun *time*
+Get-Command -noun *process*
+
+#combine
+Get-Command -noun *share* -verb Re*
+
+
+Get-Command
+
+#notice all the different types of commands
+Get-Command *service*
+#I'll refine the command
+Get-Command *service
+
+
+
+cls
