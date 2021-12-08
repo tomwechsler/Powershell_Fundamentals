8dd72fc03415766e30821da2ef1e477483c60d48
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-12-08 07:35:30 +0100

    Add files via upload

---
+++ 06_xx_Experiment.ps1
@@ -0,0 +1,9 @@
+﻿#Range operator
+1..500
+
+#Create 500 training (test) accounts
+1..500 | ForEach-Object {New-ADUser -Name "User$_" -AccountPassword (ConvertTo-SecureString -AsPlainText "DemoPass1!" -Force) -Enabled:$TRUE}
+
+
+#New-Aduser (Default cmdlet for creating a user)
+#ConvertTo-SecureString (plain text is translated to a SecureString format)
---
+++ 06_xx_Optimizing_Query_Performance.ps1
@@ -0,0 +1,29 @@
+﻿Set-Location C:\
+Clear-Host
+
+#How many AD object are there?
+Get-ADUser -Filter * | Measure-Object
+
+#A first search
+Get-ADUser -Filter * -Properties * | Where-Object {$PSItem.city -eq "Luzern"} | 
+Select-Object name,department,title
+
+#How long does this search take? We focus on the TotalSeconds
+Measure-Command {Get-ADUser -Filter * -Properties * | 
+Where-Object {$PSItem.city -eq "Luzern"} | Select-Object name,department,title}
+
+#So we get the seconds right away
+(Measure-Command {Get-ADUser -Filter * -Properties * | Where-Object {$PSItem.city -eq "Luzern"} | 
+Select-Object name,department,title}).TotalSeconds
+
+#Now we adjust our search and measure the time again
+Get-ADUser -Filter {city -eq "Luzern"} -Properties * | Select-Object name,department,title
+(Measure-Command {Get-ADUser -Filter {city -eq "Luzern"} -Properties * | 
+Select-Object name,department,title}).TotalSeconds
+
+#And again an adjustment of the search and measure again
+Get-ADUser -Filter {city -eq "Luzern"} -Properties name, department, title | Select-Object name, department, title
+(Measure-Command {Get-ADUser -Filter {city -eq "Luzern"} -Properties name, department, title | 
+Select-Object name, department, title}).TotalSeconds
+
+#The more precise you create your search, the faster you will get the result. - What would be with an AD with 50'000 objects?
