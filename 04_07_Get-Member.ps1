7867cadc817b83653ca2b0ab04713f6b26300119
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-12-16 10:57:02 +0100

    Update 04_07_Get-Member.ps1

--- 04_07_Get-Member.ps1
+++ 04_07_Get-Member.ps1
@@ -3,4 +3,4 @@ Get-Service | more
 Get-Service | Get-Member
 Get-Service | Select-Object Name,MachineName,Status
 Get-Service | Select-Object Name,MachineName,Status | Get-Member
-Get-Service | Where-Object Status -eq "Stopped" | Mores
+Get-Service | Where-Object Status -eq "Stopped" | More
